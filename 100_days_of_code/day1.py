d7b5a4a59c17c658f3da1698a5ebc3e20a75671f
hoangnym <your_email_here>
2021-05-05 21:39:21 +0200

    Start 100 days of code with the App Brewery

---
+++ 100_days_of_code/day1.py
@@ -0,0 +1,9 @@
+### create a band name generator game
+
+print("Welcome to the Band Name Generator.")
+
+city = input("What is the name of the city you grew up in?\n")
+animal = input("What is your favourite animal?\n")
+
+print(f"Your band name could be {city.strip()} {animal.strip()}.")
+
