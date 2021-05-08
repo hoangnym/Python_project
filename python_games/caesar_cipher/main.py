3293a6b976dda131b2a5df2a3cb67a8766061cee
hoangnym <hoangnym94@gmail.com>
2021-05-08 21:45:59 +0200

    Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text

--- python_games/caesar_cipher/main.py
+++ python_games/caesar_cipher/main.py
@@ -22,6 +22,21 @@ def encrypt(text, shift):
     print(f"The encypted text is {encrypted}.")
 
 #TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(text, shift):
+
+    decrypted = ''
+    for letter in text:
+        idx_o = alphabet.index(letter)
+        idx_s = idx_o - shift
+
+        ## Account for out of range error
+        if idx_s < 0:
+            idx_s += 26
+
+        decrypted += alphabet[idx_s]
+
+    print(f"The decrypted text is {decrypted}.")
+
 
   #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
   #e.g.
@@ -31,7 +46,8 @@ def encrypt(text, shift):
   #print output: "The decoded text is hello"
 
 
-#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. 
+#Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
 
 
 
