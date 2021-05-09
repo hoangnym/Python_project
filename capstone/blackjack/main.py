04acdc83000896cd62685c17a9d2ff3973acad56
hoangnym <hoangnym94@gmail.com>
2021-05-09 21:20:40 +0200

    Add print text functionality to blackjack game and recursion

--- capstone/blackjack/main.py
+++ capstone/blackjack/main.py
@@ -1,6 +1,3 @@
-from art import logo
-import random
-
 ############### Blackjack Project #####################
 
 #Difficulty Normal 😎: Use all Hints below to complete the project.
@@ -31,6 +28,8 @@ import random
 #   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
 #Then try to create your own flowchart for the program.
 
+from art import logo
+import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
@@ -64,19 +63,32 @@ def blackjack():
                 outcome = game_finished(your_cards)
                 continue_drawing = False
         elif user_input == 'n':
-            while sum(dealer_cards) <= 16:
-                dealer_cards.append(random.choice(cards))
+            if sum(your_cards) <= 21:
+                while sum(dealer_cards) <= 16:
+                    dealer_cards.append(random.choice(cards))
             continue_drawing = False
     
     ### compare dealer_cards and your_cards
     if sum(your_cards) > 21:
-        print(outcome, dealer_cards)
+        print_text(your_cards, dealer_cards)
+        print(outcome)
     elif sum(dealer_cards) > 21:
-        print(your_cards, dealer_cards, "You win.")
+        print_text(your_cards, dealer_cards)
+        print(f"Opponent went over. You win.")
+    elif sum(your_cards) > sum(dealer_cards) and sum(your_cards) == 21:
+        print_text(your_cards, dealer_cards)
+        print(outcome)
     elif sum(your_cards) > sum(dealer_cards):
-        print(your_cards, dealer_cards, "You win.")
+        print_text(your_cards, dealer_cards)
+        print("You win.")
     else:
-        print(sum(your_cards), sum(dealer_cards), "Dealer wins.")
+        print_text(your_cards, dealer_cards)
+        print("Dealer wins.")
+
+    if input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == 'y':
+        #clear window
+        print(chr(27) + "[2J")
+        blackjack()
 
     return
 
@@ -85,59 +97,26 @@ def game_finished(current_hand):
     '''
     Checks automatically whether game is finished
     '''
-    if sum(current_hand) >= 21:
+    if sum(current_hand) > 21:
         ## check whether 11 is in current_hand > replace with one
         if 11 in current_hand:
             current_hand[current_hand.index(11)] = 1
-        if sum(current_hand) >= 21:
+        if sum(current_hand) > 21:
             return "You went over. You lose."
     if sum(current_hand) == 21:
         return "Blackjack. You win."
     else:
         return False
 
+## Print text
+def print_text(current_hand, opponent_hand):
+    print(f"Your final hand: {current_hand}, final score: {sum(current_hand)}")
+    print(f"Dealer's final hand: {opponent_hand}, final score: {sum(opponent_hand)}")
+
+
 ### Start a sequence
-if input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == 'y':
-    blackjack()
-
-
-
-
-
-
-
-
-
-
-
-
-#Hint 3: Download and read this flow chart I've created:
-#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
-
-#Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
-#11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-
-#Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-#user_cards = []
-#computer_cards = []
-
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input
-#and returns the score.
-#Look up the sum() function to help you do this.
-
-#Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
-
-#Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
-
-#Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
-
-#Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
-
-#Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
-
-#Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
-
-#Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
-
-#Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
+if __name__ == "__main__":
+    if input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == 'y':
+        ### Clear window
+        print(chr(27) + "[2J")
+        blackjack()
