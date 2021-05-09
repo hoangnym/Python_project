1af208f62b87137ec455f860a66116b3040e72d8
hoangnym <hoangnym94@gmail.com>
2021-05-09 15:18:52 +0200

    Add recursive step to calculator

--- python_intermediate/calculator/main.py
+++ python_intermediate/calculator/main.py
@@ -23,24 +23,27 @@ operations = {
     "*" : multiply,
     "/" : divide, 
 }
+def calculator():
+    num1 = int(input("What is the first number?: "))
+    next_iteration = True
 
-num1 = int(input("What is the first number?: "))
-next_iteration = True
+    while next_iteration:
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation from the line above: ")
+        num2 = int(input("What is the next number?: "))
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-while next_iteration:
-    for symbol in operations:
-        print(symbol)
-    operation_symbol = input("Pick an operation from the line above: ")
-    num2 = int(input("What is the next number?: "))
-    answer = operations[operation_symbol](num1, num2)
-    print(f"{num1} {operation_symbol} {num2} = {answer}")
+        ask_for_continuation = input(f"Type 'y' to continue calculating with {answer} or type 'n' to start new calculation.: ").lower()
+        ### assign answer to new first variable
+        if ask_for_continuation == 'y':
+            num1 = answer
+        elif ask_for_continuation == 'n':
+            next_iteration = False
+            calculator()
+        else:
+            print("This is no valid answer.")
+            break
 
-    ask_for_continuation = input(f"Type 'y' to continue calculating with {answer} or type 'n' to exit.: ").lower()
-    ### assign answer to new first variable
-    if ask_for_continuation == 'y':
-        num1 = answer
-    elif ask_for_continuation == 'n':
-        next_iteration = False
-    else:
-        print("This is no valid answer.")
-        break
+calculator()
