1160fb97e1988ab16bc5ad156a593c29885f2852
hoangnym <hoangnym94@gmail.com>
2021-05-13 08:38:55 +0200

    Add prettytable module

--- python_intermediate/oop/intro.py
+++ python_intermediate/oop/intro.py
@@ -1,14 +1,21 @@
-from turtle import Turtle, Screen
-# TK_SILENCE_DEPRECATION = 1
+# from turtle import Turtle, Screen
+# # TK_SILENCE_DEPRECATION = 1
+#
+# timmy = Turtle()
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+# print(timmy)
+#
+# my_screen = Screen()
+#
+# print(my_screen.canvheight)
+#
+# my_screen.exitonclick()
 
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
-print(timmy)
 
-my_screen = Screen()
+from prettytable import PrettyTable
 
-print(my_screen.canvheight)
+table = PrettyTable()
 
-my_screen.exitonclick()
+print(table)
