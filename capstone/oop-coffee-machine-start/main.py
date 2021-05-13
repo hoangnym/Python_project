874fb72bb335e767f7b1969971a2accdbff1aabf
hoangnym <hoangnym94@gmail.com>
2021-05-13 09:20:55 +0200

    Complete oop coffee maker

--- capstone/oop-coffee-machine-start/main.py
+++ capstone/oop-coffee-machine-start/main.py
@@ -1,3 +1,31 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+# Initialize objects
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+taking_order = True
+
+while taking_order:
+    # Ask user for order
+    order = input("What would you like to drink? (espresso, latte, cappuccino): ")
+
+    # Print report
+    if order == "report":
+        coffee_maker.report()
+
+    elif order == "off":
+        taking_order = False
+
+    else:
+        # Check resources sufficient?
+        menu_item = menu.find_drink(order)
+        if coffee_maker.is_resource_sufficient(menu_item):
+            print(menu_item)
+            # Process coins and check transaction successful
+            if money_machine.make_payment(menu_item.cost):
+                # Make coffee
+                coffee_maker.make_coffee(menu_item)
