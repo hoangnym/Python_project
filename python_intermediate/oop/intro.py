3b7cc568ed5de7d44e1bbd4e0d159560dc751333
hoangnym <hoangnym94@gmail.com>
2021-05-13 08:30:36 +0200

    Create a turtle

---
+++ python_intermediate/oop/intro.py
@@ -0,0 +1,14 @@
+from turtle import Turtle, Screen
+# TK_SILENCE_DEPRECATION = 1
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+print(timmy)
+
+my_screen = Screen()
+
+print(my_screen.canvheight)
+
+my_screen.exitonclick()
