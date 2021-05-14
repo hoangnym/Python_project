dc188c0a3a803b53bab1abe5014f3e198c3ede36
hoangnym <hoangnym94@gmail.com>
2021-05-14 22:59:21 +0200

    Extract the colors and comment out colorgram function as computational intensive

--- capstone/hirst-painting/main.py
+++ capstone/hirst-painting/main.py
@@ -1,14 +1,19 @@
-import colorgram
+# import colorgram
+#
+#
+# # Get color from image
+# rgb_colors = []
+# colors = colorgram.extract('image.jpeg', 30)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
 
-
-# Get color from image
-rgb_colors = []
-colors = colorgram.extract('image.jpeg', 30)
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r, g, b)
-    rgb_colors.append(new_color)
-
-print(rgb_colors)
+rgb_colors = [(1, 12, 31), (54, 25, 17), (218, 127, 106), (9, 104, 160), (242, 213, 68), (150, 83, 39), (216, 86, 63),
+              (156, 6, 24), (165, 162, 30), (158, 62, 102), (207, 73, 103), (10, 64, 33), (11, 96, 57), (95, 6, 20),
+              (175, 134, 162), (7, 173, 217), (1, 61, 145), (2, 213, 207), (158, 32, 23), (8, 140, 85), (144, 227, 217),
+              (121, 193, 147), (220, 177, 216), (100, 218, 229), (251, 198, 1), (116, 170, 192)]
