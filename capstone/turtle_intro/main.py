3d11d2f1a1d3002ba4346c966b2efaea42b31b05
hoangnym <hoangnym94@gmail.com>
2021-05-14 20:05:52 +0200

    Let timmy draw triangle up to decagon

--- capstone/turtle_intro/main.py
+++ capstone/turtle_intro/main.py
@@ -5,11 +5,20 @@ timmy.shape("turtle")
 timmy.color("coral")
 
 # timmy draws dashed line
-for _ in range(20):
-    timmy.forward(10)
-    timmy.penup()
-    timmy.forward(10)
-    timmy.pendown()
+# for _ in range(20):
+#     timmy.forward(10)
+#     timmy.penup()
+#     timmy.forward(10)
+#     timmy.pendown()
+
+# Let timmy draw triangle up to decagon
+for i in range(3, 11):
+    angle = 360 / i
+    # Draw the figure
+    for _ in range(i):
+        timmy.forward(100)
+        timmy.right(angle)
+
 
 screen = Screen()
 screen.exitonclick()
