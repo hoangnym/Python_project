7885bf9703d8dc54d7e8f6f4339e52769d97ffb6
hoangnym <hoangnym94@gmail.com>
2021-05-14 19:59:44 +0200

    Let timmy draw a dashed line

--- capstone/turtle_intro/main.py
+++ capstone/turtle_intro/main.py
@@ -4,9 +4,11 @@ timmy = Turtle()
 timmy.shape("turtle")
 timmy.color("red")
 
-for _ in range(4):
-    timmy.forward(100)
-    timmy.right(90)
+for _ in range(20):
+    timmy.forward(10)
+    timmy.penup()
+    timmy.forward(10)
+    timmy.pendown()
 
 screen = Screen()
 screen.exitonclick()
