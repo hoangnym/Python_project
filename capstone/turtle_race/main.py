3ec83befa193aa7346ce73840eae1a8a0e658693
hoangnym <hoangnym94@gmail.com>
2021-05-15 08:32:20 +0200

    initialize race to on

--- capstone/turtle_race/main.py
+++ capstone/turtle_race/main.py
@@ -1,19 +1,33 @@
 from turtle import Turtle, Screen
+import random
+
 
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet",
                             prompt="Which turtle will win the race? Enter a color: ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+all_turtles = []
 
 y_pos = -100
 
+# Create new turtle instances
 for idx in range(len(colors)):
     turtle = Turtle(shape="turtle")
     turtle.color(colors[idx])
     turtle.penup()
     turtle.goto(x=-230, y=y_pos)
     y_pos += 40
+    all_turtles.append(turtle)
+
+is_race_on = False
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    rand_distance = random.randint(0, 10)
+    turtle.forward(rand_distance)
 
 
 screen.exitonclick()
