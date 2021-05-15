2b39244a8cf670ecabd896dc1d974df72aabd812
hoangnym <hoangnym94@gmail.com>
2021-05-15 12:11:04 +0200

    Create white square

--- capstone/snake/main.py
+++ capstone/snake/main.py
@@ -1,4 +1,21 @@
+# Import libraries
+from turtle import Screen, Turtle
+
+# Set up screen
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+
 # TODO: 1. Build snake body
+segment_1 = Turtle("square")
+segment_1.color("white")
+# x_pos = 0
+# for _ in range(3):
+#     turtle = Turtle(shape="square")
+#     turtle.color("white")
+#     turtle.goto(x_pos, 0)
+#     x_pos -= 10
 
 # TODO: 2. Move the snake
 
@@ -11,3 +28,9 @@
 # TODO: 6. Detect collision with wall
 
 # TODO: 7. Detect collision with tail
+
+
+
+
+
+screen.exitonclick()
