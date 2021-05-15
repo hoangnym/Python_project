215c2f5b2c4ba7abcf8b1f8fcb09ade772b5f60e
hoangnym <hoangnym94@gmail.com>
2021-05-15 08:36:46 +0200

    Add winning condition

--- capstone/turtle_race/main.py
+++ capstone/turtle_race/main.py
@@ -26,8 +26,19 @@ if user_bet:
     is_race_on = True
 
 while is_race_on:
-    rand_distance = random.randint(0, 10)
-    turtle.forward(rand_distance)
+
+    for turtle in all_turtles:
+        if turtle.x > 230:
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You have won! The {winning_color} turtle is the winner.")
+            else:
+                print(f"You have lost! The {winning_color} turtle is the winner.")
+        rand_distance = random.randint(0, 10)
+        turtle.forward(rand_distance)
+
+
 
 
 screen.exitonclick()
