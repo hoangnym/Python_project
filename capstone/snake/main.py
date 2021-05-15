be239359d5dc2022f8cabfd81b04ce75bcdae0ce
hoangnym <hoangnym94@gmail.com>
2021-05-15 14:59:18 +0200

    Refactor code

--- capstone/snake/main.py
+++ capstone/snake/main.py
@@ -11,14 +11,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 # TODO: 1. Build snake body
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-for position in starting_positions:
-    new_segment = Turtle(shape="square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake(3)
 
 # TODO: 2. Move the snake
 game_is_on = True
@@ -26,12 +19,7 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for idx in range(len(segments) - 1, 0, -1):
-        new_x = segments[idx - 1].xcor()
-        new_y = segments[idx - 1].ycor()
-        segments[idx].goto(new_x, new_y)
-    segments[0].forward(20)
-    segments[0].left(90)
+    snake.move()
 
 
 # TODO: 3. Create snake food
--- capstone/snake/snake.py
+++ capstone/snake/snake.py
@@ -3,15 +3,28 @@ from turtle import Turtle
 
 class Snake:
 
-    def __init__(self):
-        starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-        segments = []
-        for position in starting_positions:
+    def __init__(self, num_segs=3):
+        self.num_segs = num_segs
+        self.segments = []
+        self.starting_positions = []
+        x_pos = 0
+        y_pos = 0
+        for i in range(self.num_segs):
+            self.starting_positions.append((x_pos, y_pos))
+            x_pos -= 20
+        for position in self.starting_positions:
             new_segment = Turtle(shape="square")
             new_segment.color("white")
             new_segment.penup()
             new_segment.goto(position)
-            segments.append(new_segment)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for idx in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[idx - 1].xcor()
+            new_y = self.segments[idx - 1].ycor()
+            self.segments[idx].goto(new_x, new_y)
+        self.segments[0].forward(20)
 
 
 
