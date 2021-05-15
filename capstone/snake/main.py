e52a884bd9cce586de360176fdb76cecaec9decf
hoangnym <hoangnym94@gmail.com>
2021-05-15 14:42:29 +0200

    Let Snake move forwards

--- capstone/snake/main.py
+++ capstone/snake/main.py
@@ -1,11 +1,13 @@
 # Import libraries
 from turtle import Screen, Turtle
+import time
 
 # Set up screen
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
+screen.tracer(0)
 
 # TODO: 1. Build snake body
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
@@ -20,9 +22,15 @@ for position in starting_positions:
 # TODO: 2. Move the snake
 game_is_on = True
 while game_is_on:
-    for seg in segments:
-        seg.forward(20)
+    screen.update()
+    time.sleep(0.1)
 
+    for idx in range(len(segments) - 1, 0, -1):
+        new_x = segments[idx - 1].xcor()
+        new_y = segments[idx - 1].ycor()
+        segments[idx].goto(new_x, new_y)
+    segments[0].forward(20)
+    segments[0].left(90)
 
 
 # TODO: 3. Create snake food
