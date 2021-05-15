609b0c30f259622a467330967dfd12b7e1bfdc11
hoangnym <hoangnym94@gmail.com>
2021-05-15 06:52:17 +0200

    Fix some bugs

--- capstone/turtle_intro/etch_a_sketch.py
+++ capstone/turtle_intro/etch_a_sketch.py
@@ -1,36 +1,36 @@
 import turtle as t
 
-
-def forwards(turtle):
-    turtle.forward(10)
-
-
-def backwards(turtle):
-    turtle.backward(10)
-
-
-def counter_clockwise(turtle):
-    turtle.seth(to_angle=5)
-    turtle.forward(10)
-
-
-def clockwise(turtle):
-    turtle.seth(to_angle=355)
-    turtle.forward(10)
-
-
-def clear_drawing(turtle):
-    turtle.clearsreen()
-
-
 timmy = t.Turtle()
+
+def forwards():
+    timmy.forward(10)
+
+
+def backwards():
+    timmy.backward(10)
+
+
+def counter_clockwise():
+    timmy.right(10)
+    timmy.forward(10)
+
+
+def clockwise():
+    timmy.left(10)
+    timmy.forward(10)
+
+
+def clear_drawing():
+    timmy.clearsreen()
+
+
 screen = t.Screen()
 
-screen.listen()
 screen.onkey(key="w", fun=forwards)
 screen.onkey(key="s", fun=backwards)
 screen.onkey(key="d", fun=clockwise)
 screen.onkey(key="a", fun=counter_clockwise)
 screen.onkey(key="c", fun=clear_drawing)
+screen.listen()
 
 screen.exitonclick()
