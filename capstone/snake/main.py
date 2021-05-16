c9da1d26de4d40b2406815e24760aaa52454ecb3
hoangnym <hoangnym94@gmail.com>
2021-05-16 09:47:20 +0200

    Detect collision with tail

--- capstone/snake/main.py
+++ capstone/snake/main.py
@@ -45,11 +45,13 @@ while game_is_on:
         game_is_on = False
         scoreboard.game_over()
 
-
     # TODO: 7. Detect collision with tail
-
-
-
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
