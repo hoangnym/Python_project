24aa5fb347f40c2ea6a41f78cf81e8a871012a32
hoangnym <hoangnym94@gmail.com>
2021-05-21 19:52:23 +0200

    Create a new file using w as mode

--- capstone/read_and_write/main.py
+++ capstone/read_and_write/main.py
@@ -6,3 +6,7 @@ with open("my_file.txt", mode="a") as file:
 with open("my_file.txt") as file:
     contents = file.read()
     print(contents)
+
+# Create new file
+with open("new_file.txt", mode="w") as file:
+    file.write("New text")
