92ab58ae7cc5b9eca3fbce68bd63566652d57bbc
hoangnym <hoangnym94@gmail.com>
2021-05-21 20:50:57 +0200

    Open starting letter and get list of guest names

--- capstone/mail_merge/main.py
+++ capstone/mail_merge/main.py
@@ -1,8 +1,18 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+
+#TODO: Create a letter using starting_letter.txt
+#for each name in invited_names.txt
+#Replace the [name] placeholder with the actual name.
+#Save the letters in the folder "ReadyToSend".
+
+# Get content of letter
+with open("Input/Letters/starting_letter.txt") as file:
+    letter = file.readlines()
+
+# Get names of guests and save in list
+with open("Input/Names/invited_names.txt") as names:
+    names = names.read().splitlines()
+
+for name in names
