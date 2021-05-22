00a9d3d2bcef5eb7ded0760d81eaa938842ba8e5
hoangnym <hoangnym94@gmail.com>
2021-05-22 15:33:42 +0200

    Ask user for input

--- capstone/us-states-game-start/main.py
+++ capstone/us-states-game-start/main.py
@@ -3,15 +3,11 @@ import turtle as t
 screen = t.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
-
 screen.addshape(image)
-
 t.shape(image)
 
+answer_state = screen.textinput(title="Guess the State", prompt="What is another state's name?")
+print(answer_state)
 
-# def get_mouse_click_coor(x, y):
-#     print(x, y)
-#
-# t.onscreenclick(get_mouse_click_coor)
 
 t.mainloop()
