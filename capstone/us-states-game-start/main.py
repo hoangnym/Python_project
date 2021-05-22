2af4e060005a32b759b2aa8ea33099e89ff3289d
hoangnym <hoangnym94@gmail.com>
2021-05-22 18:04:00 +0200

    Add states to learn csv file after ending the game

--- capstone/us-states-game-start/main.py
+++ capstone/us-states-game-start/main.py
@@ -19,12 +19,18 @@ while game_is_on:
         game_is_on = False
     else:
         answer_state = screen.textinput(title=f"{score}/50 States Correct", prompt="What is another state's name?").title()
+        if answer_state == "Exit":
+            break
         if (answer_state in states) and (answer_state not in correct_answers):
             correct_answers.add(answer_state)
-            # Add city to map
             x_cor = int(df[df['state'] == answer_state].x)
             y_cor = int(df[df['state'] == answer_state].y)
             Map(answer_state, x_cor, y_cor)
+
+# states_to_learn.csv
+states_to_learn = {"states": list(states - correct_answers)}
+learn_df = pd.DataFrame(states_to_learn)
+learn_df.to_csv("states_to_learn.csv")
 
 screen.exitonclick()
 
