c39ef381fb8fdd4e61579317f497a674da27d50a
hoangnym <hoangnym94@gmail.com>
2021-05-22 07:52:33 +0200

    Do some basic calculations using pandas

--- capstone/weather/main.py
+++ capstone/weather/main.py
@@ -3,13 +3,26 @@
 #     data = weather_data.read().splitlines()
 #     print(data)
 
+# import csv
 
-import csv
+# with open("weather_data.csv") as weather_data:
+#     data = csv.reader(weather_data)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != 'temp':
+#             temperatures.append(int(row[1]))
+#     print(temperatures)
 
-with open("weather_data.csv") as weather_data:
-    data = csv.reader(weather_data)
-    temperatures = []
-    for row in data:
-        if row[1] != 'temp':
-            temperatures.append(int(row[1]))
-    print(temperatures)
+import pandas as pd
+
+data = pd.read_csv("weather_data.csv")
+print(type(data))                       # dataframe
+print(type(data["temp"]))               # series
+
+data_dict = data.to_dict()
+print(data_dict)
+
+# Calculate average temperature
+temp_list = data["temp"].to_list()
+avg_temp = sum(temp_list) / len(temp_list)
+print(avg_temp)
