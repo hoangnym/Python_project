87cc7b93c476b6d515d987805e9ab7bc9e26e5f9
hoangnym <hoangnym94@gmail.com>
2021-05-22 16:58:04 +0200

    import csv file states

--- capstone/us-states-game-start/main.py
+++ capstone/us-states-game-start/main.py
@@ -1,4 +1,5 @@
 import turtle as t
+import pandas as pd
 
 screen = t.Screen()
 screen.title("U.S. States Game")
@@ -6,8 +7,19 @@ image = "blank_states_img.gif"
 screen.addshape(image)
 t.shape(image)
 
-answer_state = screen.textinput(title="Guess the State", prompt="What is another state's name?")
+df = pd.read_csv("50_states.csv")
+states = df["state"]
+print(states)
+
+
+game_is_on = True
+correct = 0
+
+# while game_is_on:
+answer_state = screen.textinput(title=f"{correct}/50 States Correct", prompt="What is another state's name?").capitalize()
 print(answer_state)
+if correct == 50:
+    game_is_on = False
 
 
 t.mainloop()
