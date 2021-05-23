5beb0cc1d60ac70e7a274586f7041c442279932e
hoangnym <hoangnym94@gmail.com>
2021-05-23 06:23:50 +0200

    Get numbers from files 1 and 2

--- python_intermediate/list_com/main.py
+++ python_intermediate/list_com/main.py
@@ -1,13 +1,24 @@
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-# 🚨 Do Not Change the code above 👆
-
-#Write your 1 line code 👇 below:
 
 squared_numbers = [i*i for i in numbers]
 even_numbers = [i for i in numbers if i % 2 == 0]
 
-#Write your code 👆 above:
-
 print(squared_numbers)
 print(even_numbers)
+
+# Get numbers from file1 and file2 and see which contain similar numbers
+with open("file1.txt") as file1:
+    content1 = file1.read().splitlines()
+
+with open("file2.txt") as file2:
+    content2 = file2.read().splitlines()
+
+
+# turn strings into integers in each list
+content1 = [int(num) for num in content1]
+content2 = [int(num) for num in content2]
+
+
+
+
 
