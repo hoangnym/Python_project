e305652b2f6977d0428309db51db6b84ac640ebf
hoangnym <hoangnym94@gmail.com>
2021-05-23 06:38:33 +0200

    first dict comprehension lesson

---
+++ python_intermediate/dict_com/main.py
@@ -0,0 +1,10 @@
+import random
+
+names = ["Alex", "Beth", "Caroline", "Dave"]
+
+student_scores = {student:random.randint(1, 100) for student in names}
+
+passed_students = {student:score for (student, score) in student_scores.items() if score >= 60}
+
+print(student_scores)
+print(passed_students)
