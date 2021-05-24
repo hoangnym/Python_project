277f86a02da1386f1895a7c939e9db0f8e3a452e
hoangnym <hoangnym94@gmail.com>
2021-05-24 10:34:26 +0200

    Create a button in tkinter

--- capstone/tkinter/main.py
+++ capstone/tkinter/main.py
@@ -1,17 +1,22 @@
-import tkinter
+import tkinter as tk
 
-window = tkinter.Tk()
+window = tk.Tk()
 window.title("My first GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I am a label",
+my_label = tk.Label(text="I am a label",
                          font=("Arial", 24, "bold"))
-my_label.pack(expand=True)
+my_label.pack(side="left")
+my_label["text"] = "New Text"       # my_label.config(text="New Text")
 
 
+# Button
+def button_clicked():
+    print("I got clicked")
 
 
-
+button = tk.Button(text="Click Me", command=button_clicked)
+button.pack()
 
 window.mainloop()
--- capstone/tkinter/playground.py
+++ capstone/tkinter/playground.py
@@ -24,6 +24,8 @@ class Car:
     def __init__(self, **kw):
         self.make = kw.get("make")
         self.model = kw.get("model")
+        self.color = kw.get("color")
+        self.seats = kw.get("seats")
 
 
 my_car1 = Car(make="Nissan", model="GT-R")
