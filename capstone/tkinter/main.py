e3ad26cdd18a76caad5af4047de60a5c724f02b2
hoangnym <hoangnym94@gmail.com>
2021-05-24 10:36:01 +0200

    Change label when button clicked

--- capstone/tkinter/main.py
+++ capstone/tkinter/main.py
@@ -7,13 +7,13 @@ window.minsize(width=500, height=300)
 # Label
 my_label = tk.Label(text="I am a label",
                          font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+my_label.pack()
 my_label["text"] = "New Text"       # my_label.config(text="New Text")
 
 
 # Button
 def button_clicked():
-    print("I got clicked")
+    my_label["text"] = "I got clicked"
 
 
 button = tk.Button(text="Click Me", command=button_clicked)
