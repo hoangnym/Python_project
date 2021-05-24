6061cbb9b4de068773086ef26cfd786b396f8546
hoangnym <hoangnym94@gmail.com>
2021-05-24 09:00:56 +0200

    use kwargs in a function

--- capstone/tkinter/playground.py
+++ capstone/tkinter/playground.py
@@ -7,9 +7,14 @@ sum1 = add(1, 2, 3, 4, 5)
 print(sum1)
 
 # Unlimited named arguments (many keyword arguments)
-def calculate(**kwargs):
-    print(kwargs)
+def calculate(n, **kwargs):
+    # for (k, v) in kwargs.items():
+    #     print(k)
+    #     print(v)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    return n
 
 
-
-calculate(add=3, multiply=5)
+kwarg1 = calculate(2, add=3, multiply=5)
+print(kwarg1)
