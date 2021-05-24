9164a905fd8cd9168abb3c6f35c8c92123f110b3
hoangnym <hoangnym94@gmail.com>
2021-05-24 10:51:47 +0200

    Layout some modules in tkinter

--- capstone/tkinter/main.py
+++ capstone/tkinter/main.py
@@ -1,29 +1,33 @@
 import tkinter as tk
 
-window = tk.Tk()
-window.title("My first GUI Program")
-window.minsize(width=500, height=300)
 
-# Label
-my_label = tk.Label(text="I am a label",
-                         font=("Arial", 24, "bold"))
-my_label.pack()
-my_label["text"] = "New Text"       # my_label.config(text="New Text")
-
-
-# Button
 def button_clicked():
     new_text = input.get()
     my_label["text"] = new_text
 
 
+window = tk.Tk()
+window.title("My first GUI Program")
+window.minsize(width=500, height=300)
+
+# Label
+my_label = tk.Label(text="I am a label", font=("Arial", 24, "bold"))
+my_label["text"] = "New Text"
+my_label.grid(column=0, row=0)
+
+
+# Button
 button = tk.Button(text="Click Me", command=button_clicked)
-button.pack()
+button.grid(column=1, row=1)
+
+# Button
+new_button = tk.Button(text="Click Me", command=button_clicked)
+new_button.grid(column=2, row=0)
 
 
 # Entry
 input = tk.Entry(width=15)
-input.pack()
+input.grid(column=3, row=2)
 
 
 window.mainloop()
