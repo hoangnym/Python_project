3fc3350bf444dc01e02ce890ef361d0920d46903
hoangnym <hoangnym94@gmail.com>
2021-05-24 08:59:00 +0200

    Learn about kwargs in python

--- capstone/tkinter/playground.py
+++ capstone/tkinter/playground.py
@@ -1,5 +1,15 @@
+# Unlimited positional arguments
 def add(*args):
     return sum(args)
 
+
 sum1 = add(1, 2, 3, 4, 5)
 print(sum1)
+
+# Unlimited named arguments (many keyword arguments)
+def calculate(**kwargs):
+    print(kwargs)
+
+
+
+calculate(add=3, multiply=5)
