372551d9f8f9133b6c74df7cf8db870a227a1fe0
hoangnym <hoangnym94@gmail.com>
2021-05-25 19:59:57 +0200

    Add timer title

--- capstone/pomodoro/main.py
+++ capstone/pomodoro/main.py
@@ -23,12 +23,20 @@ if __name__ == '__main__':
     window.title("Pomodoro")
     window.config(padx=100, pady=100, bg=YELLOW)
 
+    # Create title
+    title = tk.Label(text="Timer", highlightthickness=0, bg=YELLOW, fg=GREEN, font=(FONT_NAME, 45, "normal"))
+    title.grid(column=1, row=0)
+
     # Create and place tomato image
     canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
     tomato_img = tk.PhotoImage(file="tomato.png")
     canvas.create_image(100, 112, image=tomato_img)
     canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-    canvas.pack()
+    canvas.grid(column=1, row=1)
+
+    # create start button
+
+
 
 
 
