36e3364a4fc9feb9d47be7a51ba69757d2dec5bc
hoangnym <hoangnym94@gmail.com>
2021-05-26 01:02:47 +0200

    Fix transitions

--- capstone/pomodoro/main.py
+++ capstone/pomodoro/main.py
@@ -8,16 +8,32 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 0.1
+SHORT_BREAK_MIN = 0.025
+LONG_BREAK_MIN = 0.05
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 
 # ---------------------------- TIMER MECHANISM -------------------------------
 def start_timer():
-    count_down(5*60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title.config(text="Break", fg=RED)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title.config(text="Break", fg=PINK)
+    else:
+        count_down(work_sec)
+        title.config(text="Work", fg=GREEN)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -31,7 +47,13 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
-
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        tick_mark.config(text=marks)
 
 # ---------------------------- UI SETUP ------------------------------- #
 if __name__ == '__main__':
@@ -59,7 +81,7 @@ if __name__ == '__main__':
     reset_btn.grid(column=2, row=2)
 
     # check mark
-    tick_mark = tk.Label(text="✔", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 25, "normal"))
+    tick_mark = tk.Label(bg=YELLOW, fg=GREEN, font=(FONT_NAME, 25, "normal"))
     tick_mark.grid(column=1, row=3)
 
 
