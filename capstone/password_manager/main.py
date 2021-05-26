6f05b4d9a25216fd8f99464fb3b86974a96fda34
hoangnym <hoangnym94@gmail.com>
2021-05-26 08:05:50 +0200

    Add website and email rows

--- capstone/password_manager/main.py
+++ capstone/password_manager/main.py
@@ -14,7 +14,21 @@ if __name__ == '__main__':
     canvas = tk.Canvas(width=200, height=200, highlightthickness=0)
     lock_img = tk.PhotoImage(file="logo.png")
     canvas.create_image(100, 100, image=lock_img)
-    canvas.pack()
+    canvas.grid(column=1, row=0)
+
+    # Website row
+    website_label = tk.Label(text="Website")
+    website_label.grid(column=0, row=1)
+
+    website_entry = tk.Entry(width=35)
+    website_entry.grid(column=1, row=1, columnspan=2)
+
+    # Email row
+    email_label = tk.Label(text="Email/Username")
+    email_label.grid(column=0, row=2)
+
+    email_entry = tk.Entry(width=35)
+    email_entry.grid(column=1, row=2, columnspan=2)
 
 
 
