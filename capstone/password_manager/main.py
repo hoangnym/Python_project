b7faaea708b37c3c3584b21046522f07c053d60c
hoangnym <hoangnym94@gmail.com>
2021-05-27 14:12:37 +0200

    Beautify code

--- capstone/password_manager/main.py
+++ capstone/password_manager/main.py
@@ -3,10 +3,13 @@ from tkinter import messagebox
 import random
 import pyperclip
 
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
-# TODO: Generate a password with certain rules
 def password_generator():
-    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+               'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C',
+               'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -25,9 +28,8 @@ def password_generator():
     # Copy pw to clipboard
     pyperclip.copy(password)
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-# TODO: Take User inputs and when "add" clicked saved to text file: (data.txt) => website | email | pw
-# clear screen after saving to screen (delete() method)
 def save():
     url = website_entry.get()
     email = email_entry.get()
