d3e42abc64f24299f23f1d648dcc6bb6dfad0288
hoangnym <hoangnym94@gmail.com>
2021-05-28 07:57:18 +0200

    Raise own errors

--- capstone/json/main.py
+++ capstone/json/main.py
@@ -21,5 +21,16 @@ else:
     content = file.read()
     print(content)
 finally:
+    # raise TypeError("This is an error that I made up.")
     file.close()
     print("File was closed.")
+
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human heights should not be over 3 meters.")
+
+bmi = weight / height ** 2
+print(bmi)
