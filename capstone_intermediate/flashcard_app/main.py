0aac0482814c469d6aac4c5a4e489f11d1e48005
hoangnym <hoangnym94@gmail.com>
2021-05-29 12:56:24 +0200

    Create next card functionality

--- capstone_intermediate/flashcard_app/main.py
+++ capstone_intermediate/flashcard_app/main.py
@@ -1,13 +1,19 @@
 import pandas as pd
 import tkinter as tk
 from tkinter import messagebox
+import random
 
 ### <-------------------- Constants & data ---------------------------> ###
 BACKGROUND_COLOR = "#B1DDC6"
 data = pd.read_csv("data/french_words.csv")
+to_learn = data.to_dict(orient="records")
 
 
-### <-------------------- UI SETUP ---------------------------> ###
+### <-------------------- Generate new random word ---------------------------> ###
+def next_card():
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_vocab, text=current_card["French"])
 
 
 ### <-------------------- UI SETUP ---------------------------> ###
@@ -21,18 +27,18 @@ if __name__ == '__main__':
     canvas = tk.Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
     front_card = tk.PhotoImage(file="images/card_front.png")
     canvas.create_image(400, 268, image=front_card)
-    canvas.create_text(400, 150, font=("Arial", 40, "italic"), text="Text")
-    canvas.create_text(400, 263, font=("Arial", 60, "bold"), text="Sub Text")
+    card_title = canvas.create_text(400, 150, font=("Arial", 40, "italic"), text="Language")
+    card_vocab = canvas.create_text(400, 263, font=("Arial", 60, "bold"), text="Word")
     canvas.grid(column=0, row=0, columnspan=2)
 
     # Red Button
     red_img = tk.PhotoImage(file="images/wrong.png")
-    red_btn = tk.Button(image=red_img, highlightthickness=0)
+    red_btn = tk.Button(image=red_img, highlightthickness=0, command=next_card)
     red_btn.grid(column=0, row=1)
 
     # Green Button
     green_img = tk.PhotoImage(file="images/right.png")
-    green_btn = tk.Button(image=green_img, highlightthickness=0)
+    green_btn = tk.Button(image=green_img, highlightthickness=0, command=next_card)
     green_btn.grid(column=1, row=1)
 
 
