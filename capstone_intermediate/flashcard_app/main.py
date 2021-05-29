c26c3be844a6c7223ddabee3f20375c1309d041d
hoangnym <hoangnym94@gmail.com>
2021-05-29 12:17:36 +0200

    Configure window

--- capstone_intermediate/flashcard_app/main.py
+++ capstone_intermediate/flashcard_app/main.py
@@ -1,2 +1,30 @@
+import pandas as pd
+import tkinter as tk
+from tkinter import messagebox
+
+
 BACKGROUND_COLOR = "#B1DDC6"
+
+
+
+### <-------------------- UI SETUP ---------------------------> ###
+if __name__ == '__main__':
+    # Setup window
+    window = tk.Tk(title="Flashy")
+
+    # Flashcard
+
+
+
+
+
+
+
+
+
+
+
+
+
+    window.mainloop()
 
