216f76614f85cab45d8d1e9c2f2cb2bded1037dd
hoangnym <hoangnym94@gmail.com>
2021-05-30 14:52:40 +0200

    Simplify opening and closing of connection

--- capstone_intermediate/automated_birthday_wisher/main.py
+++ capstone_intermediate/automated_birthday_wisher/main.py
@@ -4,17 +4,17 @@ import smtplib
 my_email = "zenlabdesigninfo@gmail.com"
 my_password = "govtK3K5KY&h23ePu^"
 
-# Establish connection
-connection = smtplib.SMTP("smtp.gmail.com")
+# Establish and close connection
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    # Secure connection
+    connection.starttls()
 
-# Secure connection
-connection.starttls()
+    # Login by providing account information
+    connection.login(user=my_email, password=my_password)
 
-# Login by providing account information
-connection.login(user=my_email, password=my_password)
-
-# Send Mail
-connection.sendmail(from_addr=my_email, to_addrs="minhhoangn0608@gmail.com", msg="Hello")
-
-# Close connection
-connection.close()
+    # Send Mail
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="minhhoangn0608@gmail.com",
+        msg="Subject:Hello\n\nThis is the body of my email."
+    )
