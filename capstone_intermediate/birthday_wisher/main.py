e803cbaa33944e4d9f50a379f09a2916955bea84
hoangnym <hoangnym94@gmail.com>
2021-05-30 18:14:53 +0200

    update birthdays.csv

--- capstone_intermediate/birthday_wisher/main.py
+++ capstone_intermediate/birthday_wisher/main.py
@@ -1,8 +1,17 @@
-##################### Extra Hard Starting Project ######################
+import pandas as pd
+import datetime as dt
 
 # 1. Update the birthdays.csv
+birthdays = pd.read_csv("birthdays.csv").to_dict(orient="records")
 
 # 2. Check if today matches a birthday in the birthdays.csv
+now = dt.datetime.now()
+day = now.day
+month = now.month
+
+for friend in birthdays:
+
+
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
