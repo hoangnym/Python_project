4363f392e23aba19d1c4ac0bbf0a442df71f3610
hoangnym <hoangnym94@gmail.com>
2021-05-30 18:38:22 +0200

    Anonymous email and password

--- capstone_intermediate/birthday_wisher/main.py
+++ capstone_intermediate/birthday_wisher/main.py
@@ -4,18 +4,17 @@ import random
 import smtplib
 
 
-def send_mail(recipient, content,
-              subject="Happy Birthday", my_email="zenlabdesigninfo@gmail.com", my_password="govtK3K5KY&h23ePu^"):
+def send_mail(sender, sender_pw, recipient, content, subject="Happy Birthday"):
     try:
         # Establish and close connection
         with smtplib.SMTP("smtp.gmail.com") as connection:
             # Secure connection
             connection.starttls()
             # Login by providing account information
-            connection.login(user=my_email, password=my_password)
+            connection.login(user=sender, password=sender_pw)
             # Send Mail
             connection.sendmail(
-                from_addr=my_email,
+                from_addr=sender,
                 to_addrs=recipient,
                 msg=f"Subject:{subject}\n\n{content}"
             )
@@ -30,17 +29,25 @@ if __name__ == '__main__':
 
     # Check if today matches a birthday in the birthdays.csv
     now = dt.datetime.now()
+    friends = []
+
+    sender_mail = input("What is your email address: ")
+    sender_pw = input("What is your email password: ")
 
     for friend in birthdays:
         if friend["month"] == now.month and friend["day"] == now.day:
+            friends.append(friend["name"])
             recipient_mail = friend["email"]
             letter_no = random.choice([1, 2, 3])
             with open(f"letter_templates/letter_{letter_no}.txt") as letter_file:
                 letter_content = letter_file.read().replace("[NAME]", friend["name"])
             # 4. Send the letter generated in step 3 to that person's email address.
-            send_mail(recipient_mail, letter_content)
+            send_mail(sender_mail, sender_pw, recipient_mail, letter_content)
 
-
+    if len(friends) > 0:
+        print(f"Emails sent to {friends}")
+    else:
+        print("No birthdays today.")
 
 
 
--- capstone_intermediate/smtp_intro/main.py
+++ capstone_intermediate/smtp_intro/main.py
@@ -2,31 +2,31 @@ import smtplib
 import datetime as dt
 import random
 
-# Account information
-my_email = "zenlabdesigninfo@gmail.com"
-my_password = "govtK3K5KY&h23ePu^"
 
-def send_mail(subject, content):
+def send_mail(sender_mail, sender_pw, subject, content):
     try:
         # Establish and close connection
         with smtplib.SMTP("smtp.gmail.com") as connection:
             # Secure connection
             connection.starttls()
             # Login by providing account information
-            connection.login(user=my_email, password=my_password)
+            connection.login(user=sender_mail, password=sender_pw)
             # Send Mail
             connection.sendmail(
-                from_addr=my_email,
+                from_addr=sender_mail,
                 to_addrs="minhhoangn0608@gmail.com",
                 msg=f"Subject:{subject}\n\n{content}"
             )
             print("Email successfully sent.")
-    except:
+    except ConnectionError:
         print("Could not establish connection")
+
 
 # Get current weekday date and time
 now = dt.datetime.now()
 day_of_week = now.weekday()
+sender = input("Your email: ")
+sender_password = input("Your email password: ")
 
 if day_of_week == 6:
     with open("quotes.txt", "r") as quotes_file:
@@ -34,7 +34,7 @@ if day_of_week == 6:
 
     random_quote = random.choice(quotes)
 
-    send_mail("Your Motivational Sunday Boost", random_quote)
+    send_mail(sender, sender_password, "Your Motivational Sunday Boost", random_quote)
 
 
 
