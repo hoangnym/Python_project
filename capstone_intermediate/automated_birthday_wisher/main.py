56ca0f3ac2c4604499e5e56bf90f52e5d2ed9386
hoangnym <hoangnym94@gmail.com>
2021-05-30 15:07:08 +0200

    Retrieve random quote from quotes.txt file

--- capstone_intermediate/automated_birthday_wisher/main.py
+++ capstone_intermediate/automated_birthday_wisher/main.py
@@ -1,20 +1,35 @@
 import smtplib
+import datetime as dt
+import random
 
-# Account information
-my_email = "zenlabdesigninfo@gmail.com"
-my_password = "govtK3K5KY&h23ePu^"
+# # Account information
+# my_email = "zenlabdesigninfo@gmail.com"
+# my_password = "govtK3K5KY&h23ePu^"
+#
+# # Establish and close connection
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     # Secure connection
+#     connection.starttls()
+#
+#     # Login by providing account information
+#     connection.login(user=my_email, password=my_password)
+#
+#     # Send Mail
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="minhhoangn0608@gmail.com",
+#         msg="Subject:Hello\n\nThis is the body of my email."
+#     )
 
-# Establish and close connection
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    # Secure connection
-    connection.starttls()
+# Get current weekday date and time
+now = dt.datetime.now()
+day_of_week = now.weekday()
 
-    # Login by providing account information
-    connection.login(user=my_email, password=my_password)
+if day_of_week == 6:
+    with open("quotes.txt", "r") as quotes_file:
+        quotes = quotes_file.read().splitlines()
 
-    # Send Mail
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="minhhoangn0608@gmail.com",
-        msg="Subject:Hello\n\nThis is the body of my email."
-    )
+    random_quote = random.choice(quotes)
+
+
+
