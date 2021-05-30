21f42b18e099004ddf27f55dc270c547d3ddfc07
hoangnym <hoangnym94@gmail.com>
2021-05-30 08:24:10 +0200

    Create flip card functionality

--- capstone_intermediate/flashcard_app/main.py
+++ capstone_intermediate/flashcard_app/main.py
@@ -7,13 +7,20 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 data = pd.read_csv("data/french_words.csv")
 to_learn = data.to_dict(orient="records")
+current_card = {}
 
 
 ### <-------------------- Generate new random word ---------------------------> ###
 def next_card():
+    global current_card
     current_card = random.choice(to_learn)
     canvas.itemconfig(card_title, text="French")
     canvas.itemconfig(card_vocab, text=current_card["French"])
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English")
+    canvas.itemconfig(card_vocab, text=current_card["English"])
+
 
 
 ### <-------------------- UI SETUP ---------------------------> ###
@@ -23,9 +30,12 @@ if __name__ == '__main__':
     window.title("Flashy")
     window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+    window.after(3000, func=flip_card)
+
     # Flashcard
     canvas = tk.Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
     front_card = tk.PhotoImage(file="images/card_front.png")
+    back_card = tk.PhotoImage(file="images/card_back.png")
     canvas.create_image(400, 268, image=front_card)
     card_title = canvas.create_text(400, 150, font=("Arial", 40, "italic"))
     card_vocab = canvas.create_text(400, 263, font=("Arial", 60, "bold"))
