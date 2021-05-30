9038d1710ba7ab853ec5248bd4f163a60e65f8e2
hoangnym <hoangnym94@gmail.com>
2021-05-30 15:12:30 +0200

    Send motivational quote to a recipient automatically

--- capstone_intermediate/automated_birthday_wisher/main.py
+++ capstone_intermediate/automated_birthday_wisher/main.py
@@ -2,24 +2,28 @@ import smtplib
 import datetime as dt
 import random
 
-# # Account information
-# my_email = "zenlabdesigninfo@gmail.com"
-# my_password = "govtK3K5KY&h23ePu^"
-#
-# # Establish and close connection
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     # Secure connection
-#     connection.starttls()
-#
-#     # Login by providing account information
-#     connection.login(user=my_email, password=my_password)
-#
-#     # Send Mail
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs="minhhoangn0608@gmail.com",
-#         msg="Subject:Hello\n\nThis is the body of my email."
-#     )
+# Account information
+my_email = "zenlabdesigninfo@gmail.com"
+my_password = "govtK3K5KY&h23ePu^"
+
+def send_mail(subject, content):
+    global my_email, my_password
+    try:
+        # Establish and close connection
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            # Secure connection
+            connection.starttls()
+            # Login by providing account information
+            connection.login(user=my_email, password=my_password)
+            # Send Mail
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs="minhhoangn0608@gmail.com",
+                msg=f"Subject:{subject}\n\n{content}"
+            )
+            print("Email successfully sent.")
+    except:
+        print("Could not establish connection")
 
 # Get current weekday date and time
 now = dt.datetime.now()
@@ -30,6 +34,8 @@ if day_of_week == 6:
         quotes = quotes_file.read().splitlines()
 
     random_quote = random.choice(quotes)
+
+    send_mail("Your Motivational Sunday Boost", random_quote)
 
 
 
