6dd8233a0741c6768f822e0530e369ae21e86ce3
hoangnym <hoangnym94@gmail.com>
2021-05-30 18:23:23 +0200

    Add account information

--- capstone_intermediate/birthday_wisher/main.py
+++ capstone_intermediate/birthday_wisher/main.py
@@ -1,25 +1,35 @@
 import pandas as pd
 import datetime as dt
 import random
+import smtplib
 
-# 1. Update the birthdays.csv
-birthdays = pd.read_csv("birthdays.csv").to_dict(orient="records")
+# 0. Account information
+my_email = "zenlabdesigninfo@gmail.com"
+my_password = "govtK3K5KY&h23ePu^"
 
-# 2. Check if today matches a birthday in the birthdays.csv
-now = dt.datetime.now()
-
-for friend in birthdays:
-    # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-    if friend["month"] == now.month and friend["day"] == now.day:
-        letter_no = random.choice([1, 2, 3])
-        with open(f"letter_templates/letter_{letter_no}.txt") as letter_file:
-            content = letter_file.read().replace("[NAME]", friend["name"])
-            print(content)
-        break
+def send_mail():
 
 
 
-# 4. Send the letter generated in step 3 to that person's email address.
+
+if __name__ == '__main__':
+    # 1. Update the birthdays.csv
+    birthdays = pd.read_csv("birthdays.csv").to_dict(orient="records")
+
+    # 2. Check if today matches a birthday in the birthdays.csv
+    now = dt.datetime.now()
+
+    for friend in birthdays:
+        # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+        if friend["month"] == now.month and friend["day"] == now.day:
+            letter_no = random.choice([1, 2, 3])
+            with open(f"letter_templates/letter_{letter_no}.txt") as letter_file:
+                content = letter_file.read().replace("[NAME]", friend["name"])
+                print(content)
+            # 4. Send the letter generated in step 3 to that person's email address.
+            send_mail()
+
+
 
 
 
