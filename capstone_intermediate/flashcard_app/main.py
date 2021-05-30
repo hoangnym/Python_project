dbcd9325e186175c27de3c70dad2541d29194d09
hoangnym <hoangnym94@gmail.com>
2021-05-30 08:28:37 +0200

    Flip card again after clicking

--- capstone_intermediate/flashcard_app/main.py
+++ capstone_intermediate/flashcard_app/main.py
@@ -14,16 +14,16 @@ current_card = {}
 def next_card():
     global current_card
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_vocab, text=current_card["French"])
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_vocab, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=front_card)
+    window.after(3000, func=flip_card)
 
 
 def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_vocab, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=back_card)
-
-
 
 
 ### <-------------------- UI SETUP ---------------------------> ###
