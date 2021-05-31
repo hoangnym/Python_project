bd2d8cf1326170e1b5e899d9aa26ae97934e6585
hoangnym <hoangnym94@gmail.com>
2021-05-31 09:19:22 +0200

    create get_quote function

--- capstone_intermediate/api_intro/kanye/main.py
+++ capstone_intermediate/api_intro/kanye/main.py
@@ -1,11 +1,12 @@
 from tkinter import *
+import requests
 
 
 def get_quote():
-    pass
-    #Write your code here.
-
-
+    response = requests.get(url="https://api.kanye.rest/")
+    data = response.json()
+    quote = data["quote"]
+    canvas.itemconfig(quote_text, text=quote)
 
 window = Tk()
 window.title("Kanye Says...")
@@ -21,6 +22,6 @@ kanye_img = PhotoImage(file="kanye.png")
 kanye_button = Button(image=kanye_img, highlightthickness=0, command=get_quote)
 kanye_button.grid(row=1, column=0)
 
-
+get_quote()
 
 window.mainloop()
