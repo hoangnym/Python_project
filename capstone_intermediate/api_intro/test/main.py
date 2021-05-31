0da34bc4934c840d7cdfa993c2b69159539a19fd
hoangnym <hoangnym94@gmail.com>
2021-05-31 09:33:02 +0200

    Get hours of sunrise and sunset

--- capstone_intermediate/api_intro/test/main.py
+++ capstone_intermediate/api_intro/test/main.py
@@ -1,12 +1,34 @@
 import requests
+from datetime import datetime
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-# 1xx Hold on, 2xx here you go, 3xx go away, 4xx you screwed up, 5xx I screwed up
+MY_LAT = 50.110924
+MY_LONG = 8.682127
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# # 1xx Hold on, 2xx here you go, 3xx go away, 4xx you screwed up, 5xx I screwed up
+# response.raise_for_status()
+#
+# data = response.json()
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+#
+# iss_position = (longitude, latitude)
+# print(iss_position)
+
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LONG,
+    "formatted": 0,
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+print(sunrise)
+print(sunset)
 
-iss_position = (longitude, latitude)
-print(iss_position)
+
+time_now = datetime.now()
+print(time_now)
