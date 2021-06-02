2e7d03152bfdfa71d23f82442ed414bea871dd8e
hoangnym <hoangnym94@gmail.com>
2021-06-02 23:26:14 +0200

    Send SMS of news in case of high fluctuations

--- capstone_intermediate/stock_news/main.py
+++ capstone_intermediate/stock_news/main.py
@@ -1,12 +1,19 @@
 import os
 import requests
 import datetime as dt
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
 
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 TODAY = dt.date.today()
 YESTERDAY = TODAY - dt.timedelta(days=1)
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ.get("TW_SID") # twilio account sid
+auth_token = os.environ.get("TW_AUTH_T") # twilio auth_token
 
 # Use https://newsapi.org
 def get_news(company=COMPANY_NAME):
@@ -26,6 +33,34 @@ def get_news(company=COMPANY_NAME):
     headlines = {article["title"]:article["description"] for article in top_articles}
 
     return headlines
+
+
+def send_sms(headline, message):
+    # Use https://www.twilio.com
+    # Send a seperate message with the percentage change and each article's title and description to your phone number.
+    # proxy_client = TwilioHttpClient()
+    # proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+    client = Client(account_sid, auth_token)  # when hosted in cloud include: http_client=proxy_client
+    message = client.messages.create(
+        body=f"Headline: {headline} \nBrief: {message}",
+        from_=os.environ.get("TW_PHONE"),  # your twilio number
+        to=os.environ.get("P_PHONE")  # your verified number
+    )
+
+    print(message.status)
+
+
+#Optional: Format the SMS message like this:
+"""
+TSLA: 🔺2%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?.
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+or
+"TSLA: 🔻5%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?.
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+"""
+
 
 
 if __name__ == '__main__':
@@ -51,26 +86,9 @@ if __name__ == '__main__':
     # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
     if stock_movement <= 0.05:
         # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
-        print(get_news())
+        top_news = get_news()
+        for (headline, content) in top_news.items():
+            send_sms(headline, content)
     else:
-        print(stock_movement, stock_yesterday, stock_today)
-
-
-
-
-
-    ## STEP 3: Use https://www.twilio.com
-    # Send a seperate message with the percentage change and each article's title and description to your phone number.
-
-
-    #Optional: Format the SMS message like this:
-    """
-    TSLA: 🔺2%
-    Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-    Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-    or
-    "TSLA: 🔻5%
-    Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-    Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-    """
+        print(stock_movement)
 
