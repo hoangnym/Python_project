581b7167abc55129ad0ac87ae4d3d975979f8ce4
hoangnym <hoangnym94@gmail.com>
2021-06-02 17:52:08 +0200

    Include environment variables

--- capstone_intermediate/api_intermediate/rain_alert/main.py
+++ capstone_intermediate/api_intermediate/rain_alert/main.py
@@ -5,20 +5,20 @@ from twilio.http.http_client import TwilioHttpClient
 import requests
 
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall?"
-api_key = "" # openweathermap api key
+api_key = os.environ.get("OWM_API_KEY") # openweathermap api key
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = "" # twilio account sid
-auth_token = "" # twilio auth_token
+account_sid = os.environ.get("TW_SID") # twilio account sid
+auth_token = os.environ.get("TW_AUTH_T") # twilio auth_token
 
 # FFM
-MY_LAT = 50.110924
-MY_LONG = 8.682127
+# MY_LAT = 50.110924
+# MY_LONG = 8.682127
 
 # NASHVILLE
-# MY_LAT = 36.166340
-# MY_LONG = -86.779068
+MY_LAT = 36.166340
+MY_LONG = -86.779068
 
 parameters = {
     "lat": MY_LAT,
@@ -37,14 +37,13 @@ twelve_hour_weather_codes = [hour["weather"][0]["id"] for hour in hourly_data][0
 will_rain = any(code < 700 for code in twelve_hour_weather_codes)
 
 if will_rain:
-    proxy_client = TwilioHttpClient()
-    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
-    client = Client(account_sid, auth_token, http_client=proxy_client)
-    message = client.messages \
-        .create(
+    #proxy_client = TwilioHttpClient()
+    #proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+    client = Client(account_sid, auth_token)    # when hosted in cloud include: http_client=proxy_client
+    message = client.messages.create(
         body="It is going to rain today. Remember to bring an ☔️.",
-        from_="", # your twilio number
-        to="" # your verified number
+        from_=os.environ.get("TW_PHONE"), # your twilio number
+        to=os.environ.get("P_PHONE") # your verified number
     )
 
     print(message.status)
