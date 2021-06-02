fc5defccc1fa49dbe041af524c0651103d1ae6ea
hoangnym <hoangnym94@gmail.com>
2021-06-02 17:00:20 +0200

    Get hourly data

--- capstone_intermediate/api_intermediate/rain_alert/main.py
+++ capstone_intermediate/api_intermediate/rain_alert/main.py
@@ -1,21 +1,20 @@
 import requests
 
+OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall?"
 api_key = "fc8716ba7941b8d080f5fc16059dd8c3"
-
 MY_LAT = 50.110924
 MY_LONG = 8.682127
 
 parameters = {
     "lat": MY_LAT,
     "lon": MY_LONG,
-    "appid": api_key
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
 }
 
-my_url = "https://api.openweathermap.org/data/2.5/onecall?"
-
-response = requests.get(url=my_url, params=parameters)
+response = requests.get(url=OWM_ENDPOINT, params=parameters)
 response.raise_for_status()
 weather_data = response.json()
-print(weather_data)
+hourly_data = weather_data["hourly"]
 
-
+print(hourly_data)
