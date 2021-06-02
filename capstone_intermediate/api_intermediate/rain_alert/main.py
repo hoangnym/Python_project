cbce7a60127639b17a7c6103a0c94711f69a578f
hoangnym <hoangnym94@gmail.com>
2021-06-02 17:07:45 +0200

    Bring umbrella?

--- capstone_intermediate/api_intermediate/rain_alert/main.py
+++ capstone_intermediate/api_intermediate/rain_alert/main.py
@@ -2,8 +2,18 @@ import requests
 
 OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall?"
 api_key = "fc8716ba7941b8d080f5fc16059dd8c3"
+
+# FFM
 MY_LAT = 50.110924
 MY_LONG = 8.682127
+
+# Bali
+# MY_LAT = -8.340539
+# MY_LONG = 115.091949
+
+# NASHVILLE
+# MY_LAT = 36.166340
+# MY_LONG = -86.779068
 
 parameters = {
     "lat": MY_LAT,
@@ -17,4 +27,8 @@ response.raise_for_status()
 weather_data = response.json()
 hourly_data = weather_data["hourly"]
 
-print(hourly_data)
+# Looping through next 12 hours of hourly data and figure out whether to bring umbrella
+twelve_hour_weather_codes = [hour["weather"][0]["id"] for hour in hourly_data][0:12]
+bring_umbrella = any(code < 700 for code in twelve_hour_weather_codes)
+
+print(bring_umbrella)
